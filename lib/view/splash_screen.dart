d60abd4c86e40fb2452796cba39b4cf0a697d0bf
Sharon Rodrigues <sr979151@gmail.com>
2023-11-19 22:55:40 +0530

    initial commit

---
+++ lib/view/splash_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_4/view/registration_screen.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 3)).then((value) {
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => RegistrationScreen()));
+    });
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              backgroundImage: AssetImage("assets/images/logo.png"),
+              radius: 70,
+              backgroundColor: Colors.amber,
+            ),
+            Text(
+              "Geeksynergy Task",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}

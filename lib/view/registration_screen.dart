acff55731813dadce51f9797c464e060892a5d5f
Sharon Rodrigues <sr979151@gmail.com>
2023-11-19 23:56:45 +0530

    registration updated

--- lib/view/registration_screen.dart
+++ lib/view/registration_screen.dart
@@ -1,12 +1,108 @@
 import 'package:flutter/material.dart';
 
-class RegistrationScreen extends StatelessWidget {
+class RegistrationScreen extends StatefulWidget {
   const RegistrationScreen({super.key});
+
+  @override
+  State<RegistrationScreen> createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  String? selectedProfession;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("registration"),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Registration",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              TextFormField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.person),
+                  hintText: "User name",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 8),
+              TextFormField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.lock),
+                  hintText: "Password",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 8),
+              TextFormField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.mail),
+                  hintText: "Email",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 8),
+              TextFormField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.phone),
+                  hintText: "Phone",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              Container(
+                height: 70,
+                width: double.infinity,
+                decoration: BoxDecoration(),
+                child: Row(
+                  children: [
+                    SizedBox(width: 10),
+                    Icon(Icons.work),
+                    SizedBox(width: 10),
+                    DropdownButton(
+                      hint: selectedProfession == null
+                          ? Text("Select Profession")
+                          : null,
+                      value: selectedProfession,
+                      items: professions.map((String profession) {
+                        return DropdownMenuItem(
+                          value: profession,
+                          child: Text(profession),
+                        );
+                      }).toList(),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          selectedProfession = newValue ?? "";
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(onPressed: () {}, child: Text("Register")),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
+
+List<String> professions = [
+  'Doctor',
+  'Engineer',
+  'Teacher',
+  'Software Developer',
+  'Artist',
+];
